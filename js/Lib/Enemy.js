617a58c0b4d7483b4a3807bc3e24fc1e4502be89
endurance21 <43696525+endurance21@users.noreply.github.com>
2019-04-03 23:32:21 +0530

    updated enemy.js

--- js/Lib/Enemy.js
+++ js/Lib/Enemy.js
@@ -12,6 +12,7 @@ class Enemy extends Tank{
             this.angle = (Direction[a].getAngle() + PI/2)%(2*PI);
             this.counter = 0;    
             this.fireBullet(); 
+  
         }
 
         if(this.position.x + this.width >= WIDTH || this.position.x <= 0){
